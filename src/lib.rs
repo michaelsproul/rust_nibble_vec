07d02a2ffaf2605568e2ae3a3423db796985d8f9
Michael Sproul <micsproul@gmail.com>
2014-12-02 19:31:35 -0800

    Join method and test cleanup.

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub struct NibbleVec {
 }
 
 impl NibbleVec {
+    /// Create an empty nibble vector.
     pub fn new() -> NibbleVec {
         NibbleVec {
             length: 0,
@@ -21,6 +22,9 @@ impl NibbleVec {
         }
     }
 
+    /// Create a nibble vector from a vector of bytes.
+    ///
+    /// Each byte is split into two 4-bit entries (MSB, LSB).
     pub fn from_byte_vec(vec: Vec<u8>) -> NibbleVec {
         let length = 2 * vec.len();
         NibbleVec {
@@ -29,23 +33,32 @@ impl NibbleVec {
         }
     }
 
+    /// Get the number of elements stored in the vector.
     pub fn len(&self) -> uint {
         self.length
     }
 
-    pub fn get(&self, idx: uint) -> Option<u8> {
+    /// Fetch a single entry from the vector.
+    ///
+    /// Guaranteed to be a value in the interval [0, 15].
+    ///
+    /// *Panics* if `idx >= self.len()`.
+    pub fn get(&self, idx: uint) -> u8 {
         if idx >= self.length {
-            return None;
+            panic!("attempted access beyond vector end. len is {}, index is {}", self.length, idx);
         }
         let vec_idx = idx / 2;
-        // If the index is even, take the first (most significant) half of the stored byte.
-        // Otherwise, take the second (least significant) half.
         match idx % 2 {
-            0 => Some(self.data[vec_idx] >> 4),
-            _ => Some(self.data[vec_idx] & 0x0f)
+            // If the index is even, take the first (most significant) half of the stored byte.
+            0 => self.data[vec_idx] >> 4,
+            // If the index is odd, take the second (least significant) half.
+            _ => self.data[vec_idx] & 0x0f
         }
     }
 
+    /// Add a single nibble to the vector.
+    ///
+    /// Only the 4 least-significant bits of the value are used.
     pub fn push(&mut self, val: u8) {
         if self.length % 2 == 0 {
             self.data.push(val << 4);
@@ -56,9 +69,10 @@ impl NibbleVec {
         self.length += 1;
     }
 
-    /// Cut the vector into two parts.
+    /// Split the vector into two parts.
     ///
-    /// All elements at or following the given index are returned in a new `NibbleVec`.
+    /// All elements at or following the given index are returned in a new `NibbleVec`,
+    /// with exactly `idx` elements remaining in this vector.
     ///
     /// *Panics* if `idx > self.len()`.
     pub fn split(&mut self, idx: uint) -> NibbleVec {
@@ -73,29 +87,16 @@ impl NibbleVec {
         }
     }
 
+    /// Split function for even *indices*.
     #[inline(always)]
     fn split_odd(&mut self, idx: uint) -> NibbleVec {
         let tail_vec_size = (self.length - idx) / 2;
-        let mut tail_vec = Vec::with_capacity(tail_vec_size);
-
-        // Copy the bytes, crossing boundaries in the original vec.
-        for i in range(idx / 2, self.data.len() - 1) {
-            // The first half is the second half of the old entry.
-            let first_half = self.data[i] & 0x0f;
-
-            // The second half is the first half of the next entry.
-            let second_half = self.data[i + 1] >> 4;
-
-            tail_vec.push((first_half << 4) | second_half);
-        }
+        let mut tail = NibbleVec::from_byte_vec(Vec::with_capacity(tail_vec_size));
 
         // If the new length is odd, get the trailing half-byte from the old vector.
         let tail_length = self.length - idx;
-
-        if tail_length % 2 == 1 {
-            let last = self.data[self.data.len() - 1] & 0x0f;
-            tail_vec.push(last << 4);
-        }
+        let take_last = tail_length % 2 == 1;
+        self.overlap_copy(idx / 2, self.data.len(), &mut tail.data, &mut tail.length, take_last);
 
         // Remove the copied bytes, being careful to skip the idx byte.
         for _ in range(idx / 2 + 1, self.data.len()) {
@@ -105,21 +106,19 @@ impl NibbleVec {
         // Update the length of the first NibbleVec.
         self.length = idx;
 
-        NibbleVec {
-            length: tail_length,
-            data: tail_vec
-        }
+        tail
     }
 
+    /// Split function for odd *indices*.
     #[inline(always)]
     fn split_even(&mut self, idx: uint) -> NibbleVec {
         // Avoid allocating a temporary vector by copying all the bytes in order, then popping them.
         let tail_vec_size = (self.length - idx) / 2;
-        let mut tail_vec = Vec::with_capacity(tail_vec_size);
+        let mut tail = NibbleVec::from_byte_vec(Vec::with_capacity(tail_vec_size));
 
         // Copy the bytes.
         for i in range(idx / 2, self.data.len()) {
-            tail_vec.push(self.data[i]);
+            tail.data.push(self.data[i]);
         }
 
         // Pop the same bytes.
@@ -127,12 +126,81 @@ impl NibbleVec {
             self.data.pop();
         }
 
-        let tail_length = self.length - idx;
+        // Update lengths.
+        tail.length = self.length - idx;
         self.length = idx;
 
+        tail
+    }
+
+    /// Copy data between the second half of self.data[start] and
+    /// self.data[end - 1]. The second half of the last entry is included
+    /// if include_last is true.
+    #[inline(always)]
+    fn overlap_copy(&self, start: uint, end: uint, vec: &mut Vec<u8>, length: &mut uint, include_last: bool) {
+        // Copy up to the first half of the last byte.
+        for i in range(start, end - 1) {
+            // The first half is the second half of the old entry.
+            let first_half = self.data[i] & 0x0f;
+
+            // The second half is the first half of the next entry.
+            let second_half = self.data[i + 1] >> 4;
+
+            vec.push((first_half << 4) | second_half);
+            *length += 2;
+        }
+
+        if include_last {
+            let last = self.data[end - 1] & 0x0f;
+            vec.push(last << 4);
+            *length += 1;
+        }
+    }
+
+    /// Append another nibble vector.
+    pub fn join(&mut self, other: &NibbleVec) {
+        // If the length is even, we can append directly.
+        if self.length % 2 == 0 {
+            self.length += other.length;
+            self.data.push_all(other.data.as_slice());
+            return;
+        }
+
+        // If the other vector is empty, bail out.
+        if other.len() == 0 {
+            return;
+        }
+
+        // If the length is odd, we have to perform an overlap copy.
+        // Copy the first half of the first element, to make the vector an even length.
+        self.push(other.get(0));
+
+        // Copy the rest of the vector using an overlap copy.
+        let take_last = other.len() % 2 == 0;
+        other.overlap_copy(0, other.data.len(), &mut self.data, &mut self.length, take_last);
+    }
+}
+
+impl Equiv<[u8]> for NibbleVec {
+    fn equiv(&self, other: &[u8]) -> bool {
+        if other.len() != self.len() {
+            return false;
+        }
+
+        for (i, x) in other.iter().enumerate() {
+            if self.get(i) != *x {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+impl Clone for NibbleVec {
+    fn clone(&self) -> NibbleVec {
         NibbleVec {
-            length: tail_length,
-            data: tail_vec
+            length: self.length,
+            data: self.data.clone()
         }
     }
 }
@@ -141,74 +209,86 @@ impl NibbleVec {
 mod test {
     use NibbleVec;
 
+    fn v8_7_6_5() -> NibbleVec {
+        NibbleVec::from_byte_vec(vec![8 << 4 | 7, 6 << 4 | 5])
+    }
+
+    fn v11_10_9() -> NibbleVec {
+        let mut result = NibbleVec::from_byte_vec(vec![11 << 4 | 10]);
+        result.push(9);
+        result
+    }
+
     #[test]
     fn get() {
-        let nv = NibbleVec::from_byte_vec(vec![4 << 4 | 8]);
-        assert_eq!(nv.get(0).unwrap(), 4u8);
-        assert_eq!(nv.get(1).unwrap(), 8u8);
+        let nv = NibbleVec::from_byte_vec(vec![3 << 4 | 7]);
+        assert_eq!(nv.get(0), 3u8);
+        assert_eq!(nv.get(1), 7u8);
     }
 
     #[test]
     fn push() {
         let mut nv = NibbleVec::new();
-        let data: Vec<u8> = vec![0, 1, 3, 5, 7, 9, 11, 15];
+        let data = vec![0, 1, 3, 5, 7, 9, 11, 15];
         for val in data.iter() {
             nv.push(*val);
         }
 
         for (i, val) in data.iter().enumerate() {
-            assert_eq!(nv.get(i).unwrap(), *val);
+            assert_eq!(nv.get(i), *val);
         }
     }
 
-    #[test]
-    fn split_even_idx() {
-        // Even index, even length.
-        let mut nv = NibbleVec::from_byte_vec(vec![
-            10 << 4 | 11,
-            12 << 4 | 13
-        ]);
-
-        let tail = nv.split(2);
-        assert_eq!(nv.len(), 2u);
-        assert_eq!(tail.len(), 2u);
-        assert_eq!(nv.get(0).unwrap(), 10u8);
-        assert_eq!(nv.get(1).unwrap(), 11u8);
-        assert_eq!(tail.get(0).unwrap(), 12u8);
-        assert_eq!(tail.get(1).unwrap(), 13u8);
-
-        // Even index, odd length.
-        nv.push(14u8);
-        let tail = nv.split(2);
-        assert_eq!(nv.len(), 2u);
-        assert_eq!(tail.len(), 1u);
-        assert_eq!(nv.get(0).unwrap(), 10u8);
-        assert_eq!(nv.get(1).unwrap(), 11u8);
-        assert_eq!(tail.get(0).unwrap(), 14u8);
+    fn split_test(  nibble_vec: &NibbleVec,
+                    idx: uint,
+                    first: Vec<u8>,
+                    second: Vec<u8>) {
+        let mut init = nibble_vec.clone();
+        let tail = init.split(idx);
+        assert!(init.equiv(first.as_slice()));
+        assert!(tail.equiv(second.as_slice()));
     }
 
     #[test]
-    fn split_odd_idx() {
-        // Odd index, even length.
-        let mut nv = NibbleVec::from_byte_vec(vec![
-            10 << 4 | 11,
-            12 << 4 | 13
-        ]);
+    fn split_even_length() {
+        let even_length = v8_7_6_5();
+        split_test(&even_length, 0, vec![], vec![8, 7, 6, 5]);
+        split_test(&even_length, 1, vec![8], vec![7, 6, 5]);
+        split_test(&even_length, 2, vec![8, 7], vec![6, 5]);
+        split_test(&even_length, 4, vec![8, 7, 6, 5], vec![]);
+    }
 
-        let tail = nv.split(3);
-        assert_eq!(nv.len(), 3u);
-        assert_eq!(tail.len(), 1u);
-        for i in range(0, nv.len()) {
-            assert_eq!(nv.get(i).unwrap(), (i + 10) as u8);
-        }
-        assert_eq!(tail.get(0).unwrap(), 13u8);
+    #[test]
+    fn split_odd_length() {
+        let odd_length = v11_10_9();
+        split_test(&odd_length, 0, vec![], vec![11, 10, 9]);
+        split_test(&odd_length, 1, vec![11], vec![10, 9]);
+        split_test(&odd_length, 2, vec![11, 10], vec![9]);
+        split_test(&odd_length, 3, vec![11, 10, 9], vec![]);
+    }
 
-        // Odd index, odd length.
-        let tail = nv.split(1);
-        assert_eq!(nv.len(), 1u);
-        assert_eq!(tail.len(), 2u);
-        assert_eq!(nv.get(0).unwrap(), 10u8);
-        assert_eq!(tail.get(0).unwrap(), 11u8);
-        assert_eq!(tail.get(1).unwrap(), 12u8);
+    fn join_test(vec1: &NibbleVec, vec2: &NibbleVec, result: Vec<u8>) {
+        let mut joined = vec1.clone();
+        joined.join(vec2);
+        assert!(joined.equiv(result.as_slice()));
+    }
+
+    #[test]
+    fn join_even_length() {
+        let v1 = v8_7_6_5();
+        let v2 = v11_10_9();
+        join_test(&v1, &v2, vec![8, 7, 6, 5, 11, 10, 9]);
+        join_test(&v1, &v1, vec![8, 7, 6, 5, 8, 7, 6, 5]);
+        join_test(&v1, &NibbleVec::new(), vec![8, 7, 6, 5]);
+        join_test(&NibbleVec::new(), &v1, vec![8, 7, 6, 5]);
+    }
+
+    #[test]
+    fn join_odd_length() {
+        let v1 = v8_7_6_5();
+        let v2 = v11_10_9();
+        join_test(&v2, &v1, vec![11, 10, 9, 8, 7, 6, 5]);
+        join_test(&v2, &v2, vec![11, 10, 9, 11, 10, 9]);
+        join_test(&v2, &NibbleVec::new(), vec![11, 10, 9]);
     }
 }
